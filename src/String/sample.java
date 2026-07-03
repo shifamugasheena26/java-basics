8fe5c88a4ceb1d49a24ed63660e1efd67f26aac4
Shifa Mugasheena.A <shifamugasheena26@gmail.com>
2026-07-03 20:14:31 +0530

    feat:Add a sample program

---
+++ src/String/sample.java
@@ -0,0 +1,25 @@
+package String;
+public class sample {
+    public static void main(String[] args){
+        String str = "shifa";
+
+//        for(int i=0;i<str.length();i++){
+//            System.out.println(str.charAt(i));
+//        }
+
+//        char[] charArray=str.toCharArray();
+//        for(int i=0;i<charArray.length;i++){
+//            System.out.println(charArray[i]);
+//        }
+
+//        char[] charArray=str.toCharArray();
+//        for(char ch:charArray){
+//            System.out.println(ch);
+//        }
+
+          char[] charArray=str.toCharArray();
+
+              System.out.println(new String (charArray));
+
+    }
+}
