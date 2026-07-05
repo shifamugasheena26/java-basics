da8a90fd576418b0b2cf9f1db3db65f5c39cc99b
Shifa Mugasheena.A <shifamugasheena26@gmail.com>
2026-07-05 11:27:13 +0530

    Feat:added count string

---
+++ src/String/CountString.java
@@ -0,0 +1,16 @@
+package String;
+
+public class CountString {
+    public static void main(String[] args){
+        String str = "Shifa Mugasheena";
+        char target = 'a';
+
+        int count =0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == target){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
